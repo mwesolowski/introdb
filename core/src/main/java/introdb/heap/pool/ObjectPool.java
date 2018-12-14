ae7d7d7460b0c92b895599c297aefbdad1404218
Michał Wesołowski <wesolowski@gmail.com>
2018-12-15 00:07:48 +0100

    ObjectPool implementation

--- core/src/main/java/introdb/heap/pool/ObjectPool.java
+++ core/src/main/java/introdb/heap/pool/ObjectPool.java
@@ -1,46 +1,72 @@
 package introdb.heap.pool;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ObjectPool<T> {
 
-	private final ObjectFactory<T> fcty;
-	private final ObjectValidator<T> validator;
-	private final int maxPoolSize;
+  private final ObjectFactory<T> fcty;
+  private final ObjectValidator<T> validator;
+  private final int maxPoolSize;
+  private final ArrayBlockingQueue<T> queue;
+  private final AtomicInteger atomicCreatedCount;
 
-	public ObjectPool(ObjectFactory<T> fcty, ObjectValidator<T> validator) {
-		this(fcty,validator,25);
-	}
-	
-	public ObjectPool(ObjectFactory<T> fcty, ObjectValidator<T> validator, int maxPoolSize) {
-		this.fcty = fcty;
-		this.validator = validator;
-		this.maxPoolSize = maxPoolSize;
-	}
-	
-	/**
-	 * When there is object in pool returns completed future,
-	 * if not, future will be completed when object is
-	 * returned to the pool.
-	 * 
-	 * @return
-	 */
-	public CompletableFuture<T> borrowObject() {
-		return new CompletableFuture<T>();
-	}	
-	
-	public void returnObject(T object) {
-	}
+  public ObjectPool(ObjectFactory<T> fcty, ObjectValidator<T> validator) {
+    this(fcty, validator, 25);
+  }
 
-	public void shutdown() throws InterruptedException {
-	}
+  public ObjectPool(ObjectFactory<T> fcty, ObjectValidator<T> validator, int maxPoolSize) {
+    this.fcty = fcty;
+    this.validator = validator;
+    this.maxPoolSize = maxPoolSize;
+    this.queue = new ArrayBlockingQueue<>(maxPoolSize);
+    this.atomicCreatedCount = new AtomicInteger(0);
+  }
 
-	public int getPoolSize() {
-		return 0;
-	}
+  /**
+   * When there is object in pool returns completed future,
+   * if not, future will be completed when object is
+   * returned to the pool.
+   *
+   * @return
+   */
+  public CompletableFuture<T> borrowObject() {
+    final var obj = queue.poll();
+    if (obj != null) {
+      return CompletableFuture.completedFuture(obj);
+    }
+    var createdCount = atomicCreatedCount.get();
+    while (createdCount < maxPoolSize) {
+      if (atomicCreatedCount.compareAndSet(createdCount, createdCount + 1)) {
+        return CompletableFuture.completedFuture(fcty.create());
+      }
+      createdCount = atomicCreatedCount.get();
+    }
+    return CompletableFuture.supplyAsync(this::takeFromQueueWithoutUncheckedException);
+  }
 
-	public int getInUse() {
-		return 0;
-	}
+  private T takeFromQueueWithoutUncheckedException() {
+    try {
+      return queue.take();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void returnObject(T object) {
+    queue.add(object);
+  }
+
+  public void shutdown() throws InterruptedException {
+  }
+
+  public int getPoolSize() {
+    return atomicCreatedCount.get();
+  }
+
+  public int getInUse() {
+    return atomicCreatedCount.get() - queue.size();
+  }
 
 }
